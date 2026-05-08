1349fa56a5b5849aa78e16459cb488260f026bdf
Ricardo Pereira <m@ricardopereira.eu>
2026-05-08 16:33:11 +0100

    Bump version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
